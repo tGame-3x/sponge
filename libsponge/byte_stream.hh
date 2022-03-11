fb951c1a3a204150735275f228cb9860e92fb850
tGame-3x <jiebao6921@gmail.com>
2022-03-11 04:49:46 -0800

    feat: byte_stream

--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -1,6 +1,7 @@
 #ifndef SPONGE_LIBSPONGE_BYTE_STREAM_HH
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
+#include <deque>
 #include <string>
 
 //! \brief An in-order byte stream.
@@ -11,13 +12,16 @@
 class ByteStream {
   private:
     // Your code here -- add private members as necessary.
-
-    // Hint: This doesn't need to be a sophisticated data structure at
-    // all, but if any of your tests are taking longer than a second,
-    // that's a sign that you probably want to keep exploring
-    // different approaches.
-
-    bool _error{};  //!< Flag indicating that the stream suffered an error.
+    std::deque<char> _buffer = {};
+    size_t _capacity = 0;
+    size_t _read_count = 0;
+    size_t _write_count = 0;
+    bool _input_ended_flag = false;
+    bool _error = false;  //!< Flag indicating that the stream suffered an error.
+                          // Hint: This doesn't need to be a sophisticated data structure at
+                          // all, but if any of your tests are taking longer than a second,
+                          // that's a sign that you probably want to keep exploring
+                          // different approaches.
 
   public:
     //! Construct a stream with room for `capacity` bytes.
